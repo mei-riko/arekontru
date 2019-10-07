ebff4a3f1d1cc1ba755b884d78587747ea778195
mei-riko <dobrovolska.marina@gmail.com>
2019-10-07 15:50:54 +0300

    inside page

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,21 @@
 import $ from 'jquery'
 $(document).ready(() =>{
+    $("a.scroll").click(function() {
+        $("html, body").animate({
+           scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+           duration: 500,
+           easing: "swing"
+        });
+        return false;
+    });
 
+    // Input mask
+    if( $('.phone').length > 0 ) {
+        $(".phone").inputmask({
+            mask: "8 999 999 99 99",
+            placeholder: " ",
+            showMaskOnHover: true
+        })
+    }
 });
